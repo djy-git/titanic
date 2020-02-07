25948dbb9ff4caef6edec0c34a0a0821801ba697
djyoon0223 <djyoon0223@gmail.com>
2020-02-07 13:09:59 +0900

    upd

--- models/env.py
+++ models/env.py
@@ -12,4 +12,3 @@ pd.set_option('display.precision', 3)
 ROOT_PATH = "/workspace/ydj/kaggle/kernel/titanic/data/"
 TRAIN_CSV_PATH, TEST_CSV_PATH = ROOT_PATH + "original/train.csv", ROOT_PATH + "original/test.csv"
 TRAIN_PROC_CSV_PATH, TEST_PROC_CSV_PATH = ROOT_PATH + "processed/train.csv", ROOT_PATH + "processed/test.csv"
-
